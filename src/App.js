d8543ca8781b0c1db87c0329752a2380aed8a2d0
Julia Glinka <julia.w.glinka@gmail.com>
2023-11-19 18:50:52 +0100

    Add buttons to page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Container from "./components/Container/Container.js";
+import Button from "./components/Button/Button.js";
 
 const App = () => {
   return (
     <Container>
       <h1>Hello world</h1>
+      <Button>Start</Button>
+      <Button>Stop</Button>
+      <Button>Reset</Button>
     </Container>
   );
 };
