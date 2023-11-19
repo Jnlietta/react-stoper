bdd8e24d177eb375c5a13caaa6ef3d70d6195940
Julia Glinka <julia.w.glinka@gmail.com>
2023-11-19 18:04:41 +0100

    Remove container

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
-import Container from "./components/Container/Container.js";
-
 const App = () => {
   return (
-    <Container>
-      <h1>Hello world</h1>
-    </Container>
+    <h1>Hello world</h1>
   );
 };
 
