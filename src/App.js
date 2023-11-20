3d373d58c2e0cddf0fbc5157dcd0bf1266e125a7
Julia Glinka <julia.w.glinka@gmail.com>
2023-11-20 19:56:24 +0100

    Add functions start stop reset, but they are not working

--- src/App.js
+++ src/App.js
@@ -2,18 +2,33 @@ import Container from "./components/Container/Container.js";
 import Button from "./components/Button/Button.js";
 import Timer from "./components/Timer/Timer.js";
 import { useState } from "react";
+import { useEffect } from "react";
 
 const App = () => {
 
-  const [time, setTime] = useState (0);
+  const [time, setTime] = useState (2);
+  const [intervalValue, setIntervalValue] = useState(null);
+
+  const startTimer = () => {
+    setIntervalValue(setInterval(()=> {setTime(prevValue => prevValue + 1)}, 1))
+  };
+
+  const stopTimer = useEffect(() => {
+    return () => {
+      if(startTimer) clearInterval(startTimer);
+    };
+  }, []);
+
+  const resetTimer = () => {
+    setTime(0);
+  };
 
   return (
     <Container>
-      <h1>Hello world</h1>
       <Timer time={time} />
-      <Button>Start</Button>
-      <Button>Stop</Button>
-      <Button>Reset</Button>
+      <Button onClick={() => startTimer}>Start</Button>
+      <Button onClick={() => stopTimer}>Stop</Button>
+      <Button onClick={() => resetTimer}>Reset</Button>
     </Container>
   );
 };
