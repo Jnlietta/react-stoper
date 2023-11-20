4eeb554945a3516a3afa44ee43ebdb122f524e2f
Julia Glinka <julia.w.glinka@gmail.com>
2023-11-20 14:14:33 +0100

    Add first type of timer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import Container from "./components/Container/Container.js";
 import Button from "./components/Button/Button.js";
+import Timer from "./components/Timer/Timer.js";
+import { useState } from "react";
 
 const App = () => {
+
+  const [time, setTime] = useState (0);
+
   return (
     <Container>
       <h1>Hello world</h1>
+      <Timer time={time} />
       <Button>Start</Button>
       <Button>Stop</Button>
       <Button>Reset</Button>
--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,8 +1,19 @@
+import styles from './Timer.module.scss'
 
+const Timer = ({ time }) => {
+    const formatTime = milliseconds => {
+        const hours = Math.floor(milliseconds / (1000 * 60 * 60)).toString().padStart(2, '0');
+        const minutes = Math.floor((milliseconds % (1000 * 60 * 60)) / (1000 * 60)).toString().padStart(2, '0');
+        const seconds = Math.floor((milliseconds % (1000 * 60)) / 1000).toString().padStart(2, '0');
+        const millisecondsFormatted= (milliseconds % 1000).toString().padStart(3, '0');
+      
+        return `${hours}:${minutes}:${seconds}.${millisecondsFormatted}`;
+    };
 
-const Timer = () => {
     return (
-        <div></div>
+        <div className={styles.timer}>
+            {formatTime(time)}
+        </div>
     );
 };
 
